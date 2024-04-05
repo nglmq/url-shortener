92ee3e291c600bd50b4fa1ffff69ac75857bca9b
mamko3b228 <am99270@gmail.com>
2024-04-05 22:04:53 +0300

    iter5 is completed

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var (
@@ -13,4 +14,22 @@ func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&FlagBaseURL, "b", "http://localhost:8080", "base url")
 	flag.Parse()
+
+	envRunAddr := os.Getenv("SERVER_ADDRESS")
+	if envRunAddr != "" {
+		FlagRunAddr = envRunAddr
+	} else if FlagRunAddr != "" {
+		FlagRunAddr = FlagRunAddr
+	} else {
+		FlagRunAddr = "localhost:8080"
+	}
+
+	envBaseURL := os.Getenv("BASE_URL")
+	if envBaseURL != "" {
+		FlagBaseURL = envBaseURL
+	} else if FlagBaseURL != "" {
+		FlagBaseURL = FlagBaseURL
+	} else {
+		FlagBaseURL = "http://localhost:8080"
+	}
 }
