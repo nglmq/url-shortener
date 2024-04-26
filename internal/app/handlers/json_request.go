fe275fe08fd4616ec85e1be31cd4ccdd1000a2c4
mamko3b228 <am99270@gmail.com>
2024-04-26 13:03:33 +0300

    iter7

--- internal/app/handlers/json_request.go
+++ internal/app/handlers/json_request.go
@@ -3,9 +3,12 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/nglmq/url-shortener/config"
 	"github.com/nglmq/url-shortener/internal/app/random"
+	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"strconv"
 )
@@ -25,21 +28,26 @@ func (us *URLShortener) JSONHandler(w http.ResponseWriter, r *http.Request) {
 	)
 
 	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST requests are allowed!", http.StatusBadRequest)
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
 	if err := json.NewDecoder(r.Body).Decode(&requestJSON); err != nil {
+		if err == io.EOF {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte("{}"))
+			return
+		}
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	//if err := validator.New().Struct(&requestJSON); err != nil {
-	//	validateErr := err.Error()
-	//
-	//	slog.Error(validateErr)
-	//	http.Error(w, "url tag is required", http.StatusBadRequest)
-	//	return
-	//}
+	if err := validator.New().Struct(&requestJSON); err != nil {
+		validateErr := err.Error()
+
+		slog.Error(validateErr)
+		http.Error(w, "url tag is required", http.StatusBadRequest)
+		return
+	}
 
 	alias := random.NewRandomURL()
 	us.URLs[alias] = requestJSON.URL
