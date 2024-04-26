a4b808a85a702e02b7cf52105a1f781a0834d644
mamko3b228 <am99270@gmail.com>
2024-04-26 12:34:00 +0300

    iter7

--- internal/app/handlers/json_request.go
+++ internal/app/handlers/json_request.go
@@ -3,17 +3,15 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/go-playground/validator/v10"
 	"github.com/nglmq/url-shortener/config"
 	"github.com/nglmq/url-shortener/internal/app/random"
 	"log"
-	"log/slog"
 	"net/http"
 	"strconv"
 )
 
 type JSONRequest struct {
-	URL string `json:"url" validate:"required,url"`
+	URL string `json:"url"`
 }
 
 type JSONResponse struct {
@@ -35,13 +33,13 @@ func (us *URLShortener) JSONHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if err := validator.New().Struct(&requestJSON); err != nil {
-		validateErr := err.Error()
-
-		slog.Error(validateErr)
-		http.Error(w, "url tag is required", http.StatusBadRequest)
-		return
-	}
+	//if err := validator.New().Struct(&requestJSON); err != nil {
+	//	validateErr := err.Error()
+	//
+	//	slog.Error(validateErr)
+	//	http.Error(w, "url tag is required", http.StatusBadRequest)
+	//	return
+	//}
 
 	alias := random.NewRandomURL()
 	us.URLs[alias] = requestJSON.URL
