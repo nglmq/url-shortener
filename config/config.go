52c8e91eaeeaa3bf3868c0ddf71ab3eb26aefa53
mamko3b228 <am99270@gmail.com>
2024-08-18 15:24:36 +0300

    add json config

--- config/config.go
+++ config/config.go
@@ -2,9 +2,15 @@ package config
 
 import (
 	"flag"
+	"log/slog"
 	"os"
 )
 
+// server_address": "localhost:8080", // аналог переменной окружения SERVER_ADDRESS или флага -a
+// "base_url": "http://localhost", // аналог переменной окружения BASE_URL или флага -b
+// "file_storage_path": "/path/to/file.db", // аналог переменной окружения FILE_STORAGE_PATH или флага -f
+// "database_dsn": "", // аналог переменной окружения DATABASE_DSN или флага -d
+// "enable_https": true // аналог переменной окружения ENABLE_HTTPS или флага -s
 // Flags for the server config
 var (
 	FlagRunAddr         string
@@ -12,6 +18,7 @@ var (
 	FlagInMemoryStorage string
 	DBConnection        string
 	EnableHTTPS         bool
+	ReadConfigFile      string
 )
 
 // ParseFlags parses the command line args and ENV variables
@@ -21,6 +28,7 @@ func ParseFlags() {
 	flag.StringVar(&FlagInMemoryStorage, "f", "", "in memory storage")
 	flag.StringVar(&DBConnection, "d", "", "postgres connection url")
 	flag.BoolVar(&EnableHTTPS, "s", false, "enable https")
+	flag.StringVar(&ReadConfigFile, "c", "", "read json config from file")
 	flag.Parse()
 
 	envRunAddr := os.Getenv("SERVER_ADDRESS")
@@ -46,5 +54,17 @@ func ParseFlags() {
 	envEnableHTTPS := os.Getenv("ENABLE_HTTPS")
 	if envEnableHTTPS != "" {
 		EnableHTTPS = true
+	}
+
+	envReadConfig := os.Getenv("CONFIG")
+	if envReadConfig != "" {
+		ReadConfigFile = envReadConfig
+	}
+
+	if ReadConfigFile != "" {
+		err := ReadJSONConfig(ReadConfigFile)
+		if err != nil {
+			slog.Info("error while reading json")
+		}
 	}
 }
