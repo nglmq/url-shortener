49d1ea905f7177b9f5c0a3549ebf1da10318dfeb
mamko3b228 <am99270@gmail.com>
2024-08-26 20:41:11 +0300

    add internal handler

--- config/json_config.go
+++ config/json_config.go
@@ -13,7 +13,7 @@ type JSONConfig struct {
 	FileStoragePath string `json:"file_storage_path"` // аналог переменной окружения FILE_STORAGE_PATH или флага -f
 	DatabaseDSN     string `json:"database_dsn"`      // аналог переменной окружения DATABASE_DSN или флага -d
 	EnableHTTPS     bool   `json:"enable_https"`      // аналог переменной окружения ENABLE_HTTPS или флага -s
-	TrustedSubnet string `json:"trusted_subnet"` // аналог переменной окружения TRUSTED_SUBNET или флага -t
+	TrustedSubnet   string `json:"trusted_subnet"`    // аналог переменной окружения TRUSTED_SUBNET или флага -t
 }
 
 // ReadJSONConfig read config from file
